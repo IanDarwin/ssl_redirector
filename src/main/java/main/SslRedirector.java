fbdd54f3824a2b17a67abf4edf37a6d937189fc0
Ian Darwin <ian@darwinsys.com>
2015-05-27 09:56:02 -0400

    Initial import of trivial SSL redirector project.

---
+++ src/main/java/main/SslRedirector.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpUtils;
+
+@SuppressWarnings("deprecation")
+@WebServlet(name = "SslRedirectorServlet",
+	description = "Trivial SSL redirector servlet",
+	urlPatterns = {"/*"})
+public class SslRedirector extends HttpServlet {
+
+	private static final long serialVersionUID = -6929090915091038163L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		String uri = HttpUtils.getRequestURL(req).toString() + '?' + req.getQueryString();
+		System.out.println("SslRedirector.doGet(): " + uri);
+		if (!uri.startsWith("http:")) {
+			throw new IllegalArgumentException("Invalid URI " + uri);
+		}
+		final String redirect = uri.replaceAll("^http:", "https:");
+		resp.sendRedirect(redirect);
+	}
+}
